d04862f223f66eb2dbdd2a697fd6bc000021d8d4
Robert Crooks <rcrooks@brightcove.com>
2020-02-03 09:50:14 -0500

    initial files

---
+++ _site/assets/js/feedback.js
@@ -0,0 +1,7 @@
+var fby = fby || [];
+fby.push(['showTab', {id: '15077', position: 'right', color: 'rgb(186, 206, 19)'}]);
+(function () {
+    var f = document.createElement('script'); f.type = 'text/javascript'; f.async = true;
+    f.src = '//cdn.feedbackify.com/f.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(f, s);
+})();
